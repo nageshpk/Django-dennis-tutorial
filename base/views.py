cf95f19e1fa4a343800a783f3b68b42a42a1b8c9
Blannc <38485662+nageshpk@users.noreply.github.com>
2023-02-05 07:04:12 +0000

    add the message object

--- base/views.py
+++ base/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -82,6 +82,14 @@ def home(request):
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+
+    if request.method == "POST":
+        message = Message.objects.create(
+          user=request.user,
+          room=room,
+          body=request.POST.get('body')
+        )
+        return redirect('room', pk=room.id)
     context = {'room': room, 'room_messages': room_messages}
     return render(request, "base/room.html", context)
 
