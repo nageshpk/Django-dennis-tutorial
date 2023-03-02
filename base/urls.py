5aaa0a91d226d3f3e2d91afad41acbc8298eed83
Blannc <38485662+nageshpk@users.noreply.github.com>
2023-03-02 15:11:41 +0000

    add more functionality

--- base/urls.py
+++ base/urls.py
@@ -15,4 +15,6 @@ urlpatterns = [
     path('delete_room/<str:pk>', views.deleteRoom, name='delete-room'),
     path('delete_message/<str:pk>', views.deleteMessage, name='delete-message'),
     path('update-user', views.updateUser, name='update-user'),
+    path('topics', views.topicsPage, name='topics'),
+    path('activity', views.activityPage, name='activity'),
 ]
